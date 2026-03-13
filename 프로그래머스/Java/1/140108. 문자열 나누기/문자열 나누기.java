c114e0e7d095f30dfb3c93735709ad8391fd49ee
kwonjiwon <kjw81024@gmail.com>
2026-03-13 10:08:47 +0900

    [level 1] Title: 문자열 나누기, Time: 0.88 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/Java/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(String s) {
+        int ans = 0;
+
+        int rootCharCnt = 1;
+        int otherCharCnt = 0;
+        char rootChar = s.charAt(0);
+
+        for (int i=1;i<s.length();i++){
+            if (rootCharCnt == 0) rootChar = s.charAt(i);
+
+//            System.out.println("* i: "+i);  
+
+            if (s.charAt(i) == rootChar) rootCharCnt++;
+            else otherCharCnt++;
+
+            if ((rootCharCnt == otherCharCnt) && rootCharCnt!=0){
+//                System.out.println("rootChar: "+ rootChar);
+//                System.out.println("rootCharCnt: "+ rootCharCnt);
+
+                ans++;
+                rootCharCnt = 0;
+                otherCharCnt = 0;
+            }
+        }
+
+        // 마지막에 제대로 문자열이 안 만들어질 때
+        if (rootCharCnt != 0) ans++;
+
+        return ans;
+        
+    }
+}
